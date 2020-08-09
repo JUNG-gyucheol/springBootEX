0056e556794216773f6282f21a6d0eb7d6b1e0c8
JungGYUCHEOL <kcpooh0202@naver.com>
2020-08-09 13:45:12 +0900

    springBoot Ex

---
+++ src/main/java/com/example/hellospring/SpringConfig.java
@@ -0,0 +1,56 @@
+package com.example.hellospring;
+
+import com.example.hellospring.aop.TimeTraceAop;
+import com.example.hellospring.repository.*;
+import com.example.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+import javax.sql.DataSource;
+import javax.xml.crypto.Data;
+
+@Configuration
+public class SpringConfig {
+
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+
+//    private DataSource dataSource;
+
+//    private EntityManager em;
+
+//    @Autowired
+//    public SpringConfig(DataSource dataSource){
+//        this.dataSource = dataSource;
+//    }
+
+//    @Autowired
+//    public SpringConfig(EntityManager em){
+//        this.em = em;
+//    }
+
+
+    @Bean
+    public MemberService memberService(){
+        return  new MemberService(memberRepository);
+    }
+//    @Bean
+//    public MemberRepository memberRepository(){
+//        return new MemoryMemberRepository();
+//        return  new JdbcMemberRepository(dataSource );
+//        return  new JdbcTemplateMemberRepository(dataSource);
+//            return  new JpaMemberRepository(em);
+//    }
+
+//    @Bean
+//    public TimeTraceAop timeTraceAop(){
+//        return new TimeTraceAop();
+//    }
+}
